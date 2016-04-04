0b8c72a421da204b0f36228096dddb7384c2cdd4
edgardo.agno@gmail.com <edgardoagno@EDGARDOs-MacBook-Air.local>
2016-04-04 16:33:55 +0100

    Support all complication families

--- Wind Times Extension/ComplicationController.swift
+++ Wind Times Extension/ComplicationController.swift
@@ -14,7 +14,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource, DataSourceCha
     // MARK: - Properties
     
     let forecastDuration = NSTimeInterval( 3 * 60 * 60 )
-    var current : CurrentObject? = nil // = getCurrentObject()
+    var current : CurrentObject? = nil
     
     // MARK: - Timeline Configuration
     
@@ -58,7 +58,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource, DataSourceCha
         var forecast = current?.forecasts.first
         
         guard let _ = forecast else {
-            let tmpl = templateForForecast(nil)
+            let tmpl = templateForForecast(nil, andComplication: complication)
             let entry = CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: tmpl)
             entries.append(entry)
             handler(entries)
@@ -67,7 +67,7 @@ class ComplicationController: NSObject, CLKComplicationDataSource, DataSourceCha
         
         while let thisForecast = forecast {
             if let thisEntryDate = thisForecast.timefrom where date.compare(thisEntryDate) == .OrderedAscending {
-                let tmpl = templateForForecast(thisForecast)
+                let tmpl = templateForForecast(thisForecast, andComplication: complication)
                 let entry = CLKComplicationTimelineEntry(date: thisEntryDate, complicationTemplate: tmpl)
                 entries.append(entry)
                 if (entries.count == limit) { break }
@@ -93,8 +93,21 @@ class ComplicationController: NSObject, CLKComplicationDataSource, DataSourceCha
     func getPlaceholderTemplateForComplication(complication: CLKComplication, withHandler handler: (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
         
-        var template : CLKComplicationTemplate? = nil
-        
+        handler(emptyTemplateForComplication(complication))
+    }
+    
+    func requestedUpdateDidBegin() {
+        //NSLog("log-requestedUpdateDidBegin")
+        if WatchSessionManager.sharedManager.isStale {
+            WatchSessionManager.sharedManager.session?.sendMessage(["command" : "getFavourites"], replyHandler: { (data : [String : AnyObject]) -> Void in
+                NSLog("log-replyHandler \(data)") }, errorHandler: nil)
+        }
+    }
+    
+    // MARK: - Data Source
+    
+    private func emptyTemplateForComplication(complication : CLKComplication) -> CLKComplicationTemplate {
+
         switch complication.family {
         case .ModularLarge :
             let tmpl = CLKComplicationTemplateModularLargeColumns()
@@ -107,71 +120,91 @@ class ComplicationController: NSObject, CLKComplicationDataSource, DataSourceCha
             tmpl.row3ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
             tmpl.row3Column1TextProvider = CLKSimpleTextProvider(text: "Wind speed")
             tmpl.row3Column2TextProvider = CLKSimpleTextProvider(text: "06h")
-            template = tmpl
-        default :
-            template = nil
+            return tmpl
+        case .ModularSmall :
+            let tmpl = CLKComplicationTemplateModularSmallStackImage()
+            tmpl.line1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
+            tmpl.line2TextProvider = CLKSimpleTextProvider(text: "speed")
+            return tmpl
+        case .CircularSmall :
+            let tmpl = CLKComplicationTemplateCircularSmallStackImage()
+            tmpl.line1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
+            tmpl.line2TextProvider = CLKSimpleTextProvider(text: "speed")
+            return tmpl
+        case .UtilitarianSmall :
+            let tmpl = CLKComplicationTemplateUtilitarianSmallFlat()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
+            tmpl.textProvider = CLKSimpleTextProvider(text: "speed")
+            return tmpl
+        case .UtilitarianLarge :
+            let tmpl = CLKComplicationTemplateUtilitarianLargeFlat()
+            tmpl.imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
+            tmpl.textProvider = CLKSimpleTextProvider(text: "Wind speed")
+            return tmpl
         }
-        
-        handler(template)
     }
-    
-    func requestedUpdateDidBegin() {
-        //NSLog("log-requestedUpdateDidBegin")
-        if WatchSessionManager.sharedManager.isStale {
-            WatchSessionManager.sharedManager.session?.sendMessage(["command" : "getFavourites"], replyHandler: { (data : [String : AnyObject]) -> Void in
-                NSLog("log-replyHandler \(data)") }, errorHandler: nil)
-        }
-    }
-    
-    // MARK: - Data Source
 
-    private func templateForForecast(forecast : ForecastObject?) -> CLKComplicationTemplate {
+    private func templateForForecast(forecast : ForecastObject?, andComplication complication : CLKComplication) -> CLKComplicationTemplate {
         //NSLog("log-templateForForecast \(forecast)")
         
         guard let forecast = forecast else {
-            let tmpl = CLKComplicationTemplateModularLargeColumns()
-            tmpl.row1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
-            tmpl.row1Column1TextProvider = CLKSimpleTextProvider(text: "Wind speed")
-            tmpl.row1Column2TextProvider = CLKSimpleTextProvider(text: "00h")
-            tmpl.row2ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
-            tmpl.row2Column1TextProvider = CLKSimpleTextProvider(text: "Wind speed")
-            tmpl.row2Column2TextProvider = CLKSimpleTextProvider(text: "03h")
-            tmpl.row3ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
-            tmpl.row3Column1TextProvider = CLKSimpleTextProvider(text: "Wind speed")
-            tmpl.row3Column2TextProvider = CLKSimpleTextProvider(text: "06h")
-            return tmpl
+            return emptyTemplateForComplication(complication)
         }
         
         var speed = ""
         if let c = current {
             speed = c.units.speed
         }
-        
-        let tmpl = CLKComplicationTemplateModularLargeColumns()
+        var imageProvider : CLKImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "N-white")!)
         if let d = forecast.direction {
-            tmpl.row1ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
+            imageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
         }
-        tmpl.row1Column1TextProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue) \(speed)")
-        tmpl.row1Column2TextProvider = CLKSimpleTextProvider(text: "\(forecast.hour)h")
         
-        if let next = forecast.next {
-            if let d = next.direction {
-                tmpl.row2ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
-            }
-            tmpl.row2Column1TextProvider = CLKSimpleTextProvider(text: "\(next.speedvalue) \(speed)")
-            tmpl.row2Column2TextProvider = CLKSimpleTextProvider(text: "\(next.hour)h")
+        switch complication.family {
+        case .ModularLarge :
+            let tmpl = CLKComplicationTemplateModularLargeColumns()
+            tmpl.row1ImageProvider = imageProvider
+            tmpl.row1Column1TextProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue) \(speed)")
+            tmpl.row1Column2TextProvider = CLKSimpleTextProvider(text: "\(forecast.hour)h")
             
-            if let nextOfNext = next.next {
-                if let d = nextOfNext.direction {
-                    tmpl.row3ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
+            if let next = forecast.next {
+                if let d = next.direction {
+                    tmpl.row2ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
                 }
-                tmpl.row3Column1TextProvider = CLKSimpleTextProvider(text: "\(nextOfNext.speedvalue) \(speed)")
-                tmpl.row3Column2TextProvider = CLKSimpleTextProvider(text: "\(nextOfNext.hour)h")
+                tmpl.row2Column1TextProvider = CLKSimpleTextProvider(text: "\(next.speedvalue) \(speed)")
+                tmpl.row2Column2TextProvider = CLKSimpleTextProvider(text: "\(next.hour)h")
+                
+                if let nextOfNext = next.next {
+                    if let d = nextOfNext.direction {
+                        tmpl.row3ImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "\(d.inverse.rawValue)-white")!)
+                    }
+                    tmpl.row3Column1TextProvider = CLKSimpleTextProvider(text: "\(nextOfNext.speedvalue) \(speed)")
+                    tmpl.row3Column2TextProvider = CLKSimpleTextProvider(text: "\(nextOfNext.hour)h")
+                }
             }
+            return tmpl
+        case .ModularSmall :
+            let tmpl = CLKComplicationTemplateModularSmallStackImage()
+            tmpl.line1ImageProvider = imageProvider
+            tmpl.line2TextProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue)")
+            return tmpl
+        case .CircularSmall :
+            let tmpl = CLKComplicationTemplateCircularSmallStackImage()
+            tmpl.line1ImageProvider = imageProvider
+            tmpl.line2TextProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue)")
+            return tmpl
+        case .UtilitarianSmall :
+            let tmpl = CLKComplicationTemplateUtilitarianSmallFlat()
+            tmpl.imageProvider = imageProvider
+            tmpl.textProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue)")
+            return tmpl
+        case .UtilitarianLarge :
+            let tmpl = CLKComplicationTemplateUtilitarianLargeFlat()
+            tmpl.imageProvider = imageProvider
+            tmpl.textProvider = CLKSimpleTextProvider(text: "\(forecast.speedvalue) \(speed)")
+            return tmpl
         }
-        
-        return tmpl
-    }    
+    }
     
     func dataSourceDidUpdate(dataSource: DataSource) {
         guard let currents = dataSource.currentObjects else { return }
